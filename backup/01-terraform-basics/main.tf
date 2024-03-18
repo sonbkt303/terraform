21b8ee78a9f48384d39f687cbf5cb8878cc35bff
mike bui <mike.bui.0031>
2024-03-18 17:15:22 +0700

    Update terraform

--- backup/01-terraform-basics/main.tf
+++ backup/01-terraform-basics/main.tf
@@ -1,19 +1,24 @@
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 
-  
+
 }
 
 # plan - execute
 resource "aws_s3_bucket" "my_s3_bucket" {
-    bucket = "my-s3-bucket-coconut-bucket-0809"
-    versioning {
-        enabled = true
-    }
+  bucket = "my-s3-bucket-coconut-bucket-0809"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+
+  force_destroy = false
+  object_lock_enabled = false
 }
 
 resource "aws_iam_user" "my_iam_user" {
-    name = "my_iam_user_abc_updated"
+  name = "my_iam_user_updated"
 }
 
 
--- backup/01-terraform-basics/outputs.tf
+++ backup/01-terraform-basics/outputs.tf
@@ -1,8 +1,8 @@
 
 output "my_s3_bucket_complete_details" {
-    value =  aws_s3_bucket.my_s3_bucket
+  value = aws_s3_bucket.my_s3_bucket
 }
 
 output "my_iam_user_complete_details" {
-    value = aws_iam_user.my_iam_user
+  value = aws_iam_user.my_iam_user
 }
