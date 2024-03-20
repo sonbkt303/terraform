039d0efeb31677a8957020540b18b5cab456a2f2
mike EHU8RD8 <mike.bui@gmail.com>
2024-03-20 22:39:56 +0700

    Update git ignore

---
+++ backup/08-terraform-general/main.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# plan - execute
+resource "aws_s3_bucket" "my_s3_bucket" {
+  bucket = "my-s3-bucket-coconut-bucket-0809"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+
+
+  force_destroy       = false
+  object_lock_enabled = false
+}
+
+resource "aws_iam_user" "my_iam_user" {
+  name = "my_iam_user_updated"
+  path = "/system/test/"
+}
+
+
+
+# STATE
+# DESIRED - KNOWN - ACTUAL
+
+#   access_key = "AKIAWHMZVV7EM7QNEWPR"
+#   secret_key = "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"
