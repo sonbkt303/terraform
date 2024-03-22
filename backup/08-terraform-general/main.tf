c0520c3840c77e47107489e404b0e366431127c9
mike bui <mike.bui.0031>
2024-03-22 17:03:03 +0700

    Update terraform

--- backup/08-terraform-general/main.tf
+++ backup/08-terraform-general/main.tf
@@ -1,61 +1,99 @@
 provider "aws" {
   region = "us-east-1"
 }
-variable "users" {
-  # default = ["ravs", "sats", "ranga", "tom", "jane"]
 
-  default = {
-    ravs: {
-      country: "Netherlands",
-    }
-    tom: {
-      country: "US"
-    },
-    jane: {
-      country: "India"
-    }
-    # "India"
+
+resource "aws_security_group" "http_server_sg" {
+  name   = "http_server_sg"
+  vpc_id = "vpc-aabcfbd0"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-}
 
-variable "my_iam_user_prefix" {
-  type    = string
-  default = "my_iam_user"
-}
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-variable "environment" {
-  default = "dev"
-}
-
-# plan - execute
-resource "aws_s3_bucket" "my_s3_bucket" {
-  bucket = "my-s3-bucket-coconut-bucket-0809"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-
-
-  force_destroy       = false
-  object_lock_enabled = false
-}
-
-resource "aws_iam_user" "my_iam_user" {
-  # count = length(var.names)
-  # name = "${var.environment}_${var.names[count.index]}"
-  # path = "/system/test/"
-  for_each = var.users
-  name = each.key
-  tags = {
-    country: each.value.country
+    name = "http_server_sg"
   }
 }
+
+resource "aws_instance" "http_server" {
+  ami = "ami-0c101f26f147fa7fd"
+  instance_type = "t2.micro"
+  key_name = "evn0031_ec2"
+  vpc_security_group_ids=[aws_security_group.http_server_sg.id]
+  subnet_id = "subnet-893203a7"
+  
+}
+# variable "users" {
+#   # default = ["ravs", "sats", "ranga", "tom", "jane"]
+
+#   default = {
+#     ravs: {
+#       country: "Netherlands",
+#     }
+#     tom: {
+#       country: "US"
+#     },
+#     jane: {
+#       country: "India"
+#     }
+#     # "India"
+#   }
+# }
+
+# variable "my_iam_user_prefix" {
+#   type    = string
+#   default = "my_iam_user"
+# }
+
+# variable "environment" {
+#   default = "dev"
+# }
+
+# # plan - execute
+# resource "aws_s3_bucket" "my_s3_bucket" {
+#   bucket = "my-s3-bucket-coconut-bucket-0809"
+
+#   tags = {
+#     Name        = "My bucket"
+#     Environment = "Dev"
+#   }
+
+
+#   force_destroy       = false
+#   object_lock_enabled = false
+# }
+
+# resource "aws_iam_user" "my_iam_user" {
+#   # count = length(var.names)
+#   # name = "${var.environment}_${var.names[count.index]}"
+#   # path = "/system/test/"
+#   for_each = var.users
+#   name = each.key
+#   tags = {
+#     country: each.value.country
+#   }
+# }
 
 
 
 # STATE
 # DESIRED - KNOWN - ACTUAL
 
-#   access_key = "AKIAWHMZVV7EM7QNEWPR"
-#   secret_key = "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"
--- backup/08-terraform-general/outputs.tf
+++ backup/08-terraform-general/outputs.tf
@@ -1,8 +1,13 @@
 
-output "my_s3_bucket_complete_details" {
-  value = aws_s3_bucket.my_s3_bucket
-}
+# output "my_s3_bucket_complete_details" {
+#   value = aws_s3_bucket.my_s3_bucket
+# }
 
-output "my_iam_user_complete_details" {
-  value = aws_iam_user.my_iam_user
+# output "my_iam_user_complete_details" {
+#   value = aws_iam_user.my_iam_user
+# }
+
+
+output "aws_security_group_http_server_details" {
+  value = aws_security_group.http_server_sg
 }
