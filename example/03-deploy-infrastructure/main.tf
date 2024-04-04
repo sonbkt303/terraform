29939b119c21141a388e6acec0af123191c504b2
mike bui <mike.bui.0031>
2024-04-04 17:13:03 +0700

    Update erafrom

--- example/03-deploy-infrastructure/main.tf
+++ example/03-deploy-infrastructure/main.tf
@@ -15,3 +15,20 @@ module "networking" {
   public_subnets   = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
   database_subnets = ["10.0.7.0/24", "10.0.8.0/24", "10.0.9.0/24"]
 }
+
+module "database" {
+  source = "./modules/database"
+
+  project = local.project
+  vpc     = module.networking.vpc
+  sg      = module.networking.sg
+}
+
+module "autoscaling" {
+  source = "./modules/autoscaling"
+
+  project   = local.project
+  vpc       = module.networking.vpc
+  sg        = module.networking.sg
+  db_config = module.database.config
+}
--- example/03-deploy-infrastructure/modules/networking/main.tf
+++ example/03-deploy-infrastructure/modules/networking/main.tf
@@ -1,18 +1,34 @@
+# provider "aws" {
+#   region = "us-west-2"
+# }
+
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "3.12.0"
+  version = "5.6.0"
 
-  name    = "${var.project}-vpc"
-  cidr    = var.vpc_cidr
+  name = "${var.project}-vpc"
+  cidr = var.vpc_cidr
   # azs     = data.aws_availability_zones.available.names
+  #  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  azs = ["us-west-2a", "us-west-2b", "us-west-2c"]
 
   private_subnets  = var.private_subnets
   public_subnets   = var.public_subnets
   database_subnets = var.database_subnets
 
+  # name = "terraform-series"
+  # cidr = "10.0.0.0/16"
+  # azs  = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+
+  # private_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  # public_subnets   = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  # database_subnets = ["10.0.7.0/24", "10.0.8.0/24", "10.0.9.0/24"]
+
   create_database_subnet_group = true
   enable_nat_gateway           = true
   single_nat_gateway           = true
+
 }
 
 module "alb_sg" {
@@ -31,8 +47,8 @@ module "web_sg" {
   vpc_id = module.vpc.vpc_id
   ingress_rules = [
     {
-      port        = 80
-      security_groups = [module.lb_sg.security_group.id]
+      port            = 80
+      security_groups = [module.alb_sg.security_group.id]
     }
   ]
 }
