70bf3946dc1be378cd2203ff61065534514807fb
mike bui <mike.bui.0031>
2024-04-05 10:44:49 +0700

    Update terraform

--- example/03-deploy-infrastructure/modules/database/main.tf
+++ example/03-deploy-infrastructure/modules/database/main.tf
@@ -1,14 +1,14 @@
 resource "random_password" "password" {
   length           = 16
-  special          = true
+  special          = false
   override_special = "_%@"
 }
 
 resource "aws_db_instance" "database" {
-  allocated_storage      = 20
+  allocated_storage      = 5
   engine                 = "postgres"
-  engine_version         = "12.7"
-  instance_class         = "db.t2.micro"
+#   engine_version         = "14.1"
+  instance_class         = "db.t3.micro"
   identifier             = "${var.project}-db-instance"
   db_name                = "series"
   username               = "series"
@@ -16,4 +16,6 @@ resource "aws_db_instance" "database" {
   db_subnet_group_name   = var.vpc.database_subnet_group
   vpc_security_group_ids = [var.sg.db]
   skip_final_snapshot    = true
+
+  
 }
