ec7f1ddc2ed993432ede8e67dd438181bd9c9d38
mike bui <mike.bui.0031>
2024-04-10 14:42:54 +0700

    Update

---
+++ example/07-ab-testing-deployment/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+output "dns" {
+  value = aws_cloudfront_distribution.s3_distribution.domain_name
+}
+
+output "s3" {
+  value = {
+    pro     = aws_s3_bucket.s3_pro.bucket_domain_name
+    pre_pro = aws_s3_bucket.s3_pre_pro.bucket_domain_name
+  }
+}
